7b5f9e2324106c5455fe05dab37ee7ca1340d412
setu102 <94671112+setu102@users.noreply.github.com>
2026-02-06 17:51:53 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || "")
   },
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
